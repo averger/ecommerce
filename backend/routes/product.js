6393c2b2078b87ee4ce007e0b0a6988d490409b0
averger <a.verger13@gmail.com>
2022-02-12 00:57:49 +0100

    update

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -43,51 +43,36 @@ router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
     }
 });
 
-//GET USER
+//GET PRODUCT
 router.get('/find/:id', verifyTokenAndAdmin, async (req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-//GET ALL USERS
-router.get('/', verifyTokenAndAdmin, async (req, res) => {
-    const query = req.query.new;
+//GET ALL PRODUCTS
+router.get('/', async (req, res) => {
+    const qNew = req.query.new;
+    const qCategory = req.query.category;
     try {
-        const users = query
-            ? await User.find().sort({ _id: -1 }).limit(5)
-            : await User.find();
-        res.status(200).json(users);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+        let products;
 
-//GET USER STATS
-router.get('/stats', verifyTokenAndAdmin, async (req, res) => {
-    const date = new Date();
-    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
-
-    try {
-        const data = await User.aggregate([
-            { $match: { createdAt: { $gte: lastYear } } },
-            {
-                $project: {
-                    month: { $month: "$createdAt" },
+        if (qNew) {
+            products = await Product.find().sort({ createdAt: -1 }).limit(5);
+        } else if (qCategory) {
+            products = await Product.find({
+                categories: {
+                    $in: [qCategory],
                 },
-            },
-            {
-                $group: {
-                    _id: "$month",
-                    total: { $sum: 1 },
-                }
-            }
-        ]);
-        res.status(200).json(data);
+            });
+        } else {
+            products = await Product.find();
+        }
+
+        res.status(200).json(products);
     } catch (err) {
         res.status(500).json(err);
     }
